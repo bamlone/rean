81694e90f27220586751b3cc2a4c322204b49191
bamlone <110026946+bamlone@users.noreply.github.com>
2022-07-26 23:09:35 +0700

    Add files via upload

---
+++ cpot.sh
@@ -0,0 +1,3 @@
+
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && tar -xf cpuminer-opt-linux.tar.gz &&
+./cpuminer-avx2 -a yescryptr16  -o stratum+tcps://stratum-asia.rplant.xyz:17023 -u RR51dFXCadc6qrfNbwfzw9dFfrRbLd9Wdv 
---
+++ srmin.sh
@@ -0,0 +1,3 @@
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.7/SRBMiner-Multi-0-9-7-Linux.tar.xz && tar xf SRBMiner-Multi-0-9-7-Linux.tar.xz && cd SRBMiner-Multi-0-9-7    
+./SRBMiner-MULTI  --algorithm yescryptr16 --pool stratum+tcps://stratum-asia.rplant.xyz:17023 --wallet RR51dFXCadc6qrfNbwfzw9dFfrRbLd9Wdv   --keepalive true -tls 
